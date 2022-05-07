44b859ec7b7b960ef7b7c8d7b043060d936a2b9e
elidvenega <venegaelid@gmail.com>
2022-05-06 23:06:51 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [Users, fetchUsers] = useState([]);
+
+  const getData = async () => {
+    try {
+      const url = await fetch("https://jsonplaceholder.typicode.com/users");
+      const resp = await url.json();
+      const data = await resp;
+      console.log(data);
+      fetchUsers(data);
+    } catch {
+      console.log("error");
+    }
+  };
+
+  // Works as component did mount
+  useEffect(() => {
+    getData();
+  }, []);
+  // Problem exist in line 24
+  // Map is returning {} data instead of [] data
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+      <h2>React Fetch API Example</h2>
+      <ul>
+        {/* returns a new array */}
+        {Users.map((item, i) => {
+          return (
+            <li key={i}>
+              Username:{item.username}, Email:{item.email}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
